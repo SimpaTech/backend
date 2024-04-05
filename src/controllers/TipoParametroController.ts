2d84e62647220ecbe3e669e91a230b8f141e0bde
Igor Pereira <igorpereir28@hotmail.com>
2024-04-05 11:59:46 -0300

    Listagem de tipo parametro finalizada

--- src/controllers/TipoParametroController.ts
+++ src/controllers/TipoParametroController.ts
@@ -1,5 +1,5 @@
 import { TipoParametro } from "../entities/TipoParametro"
-import { adicionarTipoParametro } from "../services/TipoParametroServices"
+import { adicionarTipoParametro, listarTodosTipoParametro, procurarTipoParametroId, procurarTipoParametro } from "../services/TipoParametroServices"
 
 class TipoParametroController {
 
@@ -27,6 +27,49 @@ class TipoParametroController {
 
             const novoTipoParametro = adicionarTipoParametro(data.Fator, data.Offset, data.Unidade, data.Nome_Tipo_Parametro);
             return res.status(200).json({ message: "Cadastrado com sucesso", novoTipoParametro });
+        } catch(error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+
+    async listarTipoParametro(req, res) {
+        try {
+            const tipoparametro = await listarTodosTipoParametro();
+            return res.json(tipoparametro).status(200);
+        } catch(error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+
+    async buscarTipoParametro(req, res) {
+        try {
+            const id = req.params.id;
+            const tipoparametro = await procurarTipoParametroId(id);
+            if (!tipoparametro) {
+                return res.json({ message: "Tipo de parâmetro não encontrado!" }).status(404);
+            } else {
+                return res.json(tipoparametro).status(200);
+            }
+        } catch(error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+
+    async filtrarTipoParametro(req, res) {
+        try {
+            const filtro = req.body;
+    
+            if (Object.keys(filtro).length === 0) {
+                return res.status(400).json({ message: "É necessário fornecer pelo menos um parâmetro de filtro." });
+            }
+    
+            const tipoparametro = await procurarTipoParametro(filtro);
+    
+            if (tipoparametro.length === 0) {
+                return res.status(404).json({ message: "Nenhum tipo de parâmetro encontrado com os filtros fornecidos." });
+            } else {
+                return res.status(200).json(tipoparametro);
+            }
         } catch(error) {
             return res.status(500).json({ error: error.message });
         }
--- src/routes/TipoParametroRoutes.ts
+++ src/routes/TipoParametroRoutes.ts
@@ -4,5 +4,8 @@ import { TipoParametroController } from "../controllers";
 const routes = Router();
 
 routes.post("/cadastrarTipoParametro", TipoParametroController.cadastrarTipoParametro);
+routes.get("/", TipoParametroController.listarTipoParametro);
+routes.get("/buscarTipoParametro/:id", TipoParametroController.buscarTipoParametro);
+routes.get("/filtrarTipoParametro", TipoParametroController.filtrarTipoParametro);
 
 export default routes;
