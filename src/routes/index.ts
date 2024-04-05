858f0300e32609e0c7cd672d629748c87fbfe4b3
Vitor Garcez <vitaoog@gmail.com>
2024-04-05 10:59:59 -0300

    Adição API para criar tipoAlerta

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { default as UsuarioRoutes } from "./UsuarioRoutes";
 import { default as EstacaoRoutes } from "./EstacaoRoutes";
+import { default as TipoAlertaRoutes }from "./TipoAlertaRoutes";
 
 const router = Router();
 
 router.use("/usuario", UsuarioRoutes);
 router.use("/estacao", EstacaoRoutes);
+router.use("/tipoAlerta", TipoAlertaRoutes);
 
 export default router;
