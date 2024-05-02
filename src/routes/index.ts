a33f3b7f21215e5cb4b370ad6ec1c44264b736c8
Vitor Garcez <vitaoog@gmail.com>
2024-05-01 22:08:23 -0300

    FEAT - Adição API recebimento dados MongoDB

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ import { default as TipoParametroRoutes }from "./TipoParametroRoutes";
 import { default as ParametroAlertaRoutes } from "./ParametroAlertaRoutes";
 import { default as OcorrenciasRoutes } from "./OcorrenciasRoutes";
 import { default as ParametroRoutes } from "./ParametroRoutes";
+import { default as MongoRoutes } from "./MongoRoutes"
 
 const router = Router();
 
@@ -17,5 +18,6 @@ router.use("/tipoParametro", TipoParametroRoutes);
 router.use("/parametroAlerta", ParametroAlertaRoutes);
 router.use("/ocorrencias", OcorrenciasRoutes);
 router.use("/parametro", ParametroRoutes);
+router.use("/mongodb", MongoRoutes);
 
 export default router;
