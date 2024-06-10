f811083b9e5b5d3ea9279dfbe7ea3c32c490fc8e
Vitor Garcez <vitaoog@gmail.com>
2024-06-10 20:57:36 -0300

    chore: Alteração na migração para apresentação devops

--- src/migrations/1717988913330-v1-0-1.ts
+++ src/migrations/1717988913330-v1-0-1.ts
@@ -4,9 +4,9 @@ export class V1011717988913330 implements MigrationInterface {
     name = 'V1011717988913330'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`estacao\` ADD \`Data_Instalacao\` datetime NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`estacao\` ADD \`Tipo_Estacao\` varchar(255) NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`estacao\` ADD \`Indicativo_Ativa\` tinyint NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`estacao\` ADD \`Data_Instalacao\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP`);
+        await queryRunner.query(`ALTER TABLE \`estacao\` ADD \`Tipo_Estacao\` varchar(255) NOT NULL DEFAULT 'Estação Terrestre'`);
+        await queryRunner.query(`ALTER TABLE \`estacao\` ADD \`Indicativo_Ativa\` tinyint NOT NULL DEFAULT 1`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -14,5 +14,4 @@ export class V1011717988913330 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`estacao\` DROP COLUMN \`Tipo_Estacao\``);
         await queryRunner.query(`ALTER TABLE \`estacao\` DROP COLUMN \`Data_Instalacao\``);
     }
-
 }
